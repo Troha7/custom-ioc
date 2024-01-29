27a2247893b57e21251ad28dcff96cee95e83faf
Dmytro Trotsenko <trohae7@gmail.com>
2024-01-29 23:00:29 +0200

    add beanDependencies field to BeanDefinition

--- src/main/java/ua/trotsenko/di/bean/definition/BeanDefinition.java
+++ src/main/java/ua/trotsenko/di/bean/definition/BeanDefinition.java
@@ -1,7 +1,11 @@
 package ua.trotsenko.di.bean.definition;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  * {@link BeanDefinition}
@@ -11,9 +15,15 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
+@Setter
 public class BeanDefinition {
 
   private final String name;
   private final Class<?> beanType;
+  private final Map<String, Field> beanDependencies = new HashMap<>();
+
+  public void addBeanDependency(String beanName, Field field){
+    this.beanDependencies.put(beanName, field);
+  }
 
 }
